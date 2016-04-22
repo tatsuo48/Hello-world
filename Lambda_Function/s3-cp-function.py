32e1dfff12cc1a1e56b2e23b39c1946cedce2f3b
root <root@ip-192-168-0-191.ap-northeast-1.compute.internal>
2016-04-22 06:16:40 +0000

    sakusei

---
+++ Lambda_Function/s3-cp-function.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import json
+import urllib
+import boto3
+
+print('Loading function')
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+        #print("Received event: " + json.dumps(event, indent=2))
+
+        # Get the object from the event and show its content type
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
+        print ("buket:" + bucket)
+        print ("key:" + key)
+        obj = s3.get_object(Bucket=bucket,Key=key)
+        body = obj['Body'].read()
+        f = 
